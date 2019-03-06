0b675b51c6ec423d845195e267050adde505b1c4
Bjørn Olav <bojalborg@gmail.com>
2019-03-06 14:09:20 +0100

    Import in build.gradle. Add Forecast to MainActivity

--- app/src/main/java/com/example/helse/MainActivity.kt
+++ app/src/main/java/com/example/helse/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.helse
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.helse.forecast.AirqualityForecast
+import com.example.helse.forecast.AirqualityForecastApiImpl
+import com.example.helse.forecast.AirqualityForecastRepositoryImpl
+import com.example.helse.forecast.AirqualityForecastViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var notificationService: NotificationService
+    private lateinit var text: String
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         notificationService = NotificationService(this)
 
+        val viewModel = ViewModelProviders.of(this).get(AirqualityForecastViewModel::class.java)
+            .apply { airqualityForecastRepository = AirqualityForecastRepositoryImpl(AirqualityForecastApiImpl()) }
+
+        viewModel.getAirqualityForecast().observe(
+            this,
+            Observer { airqualityForecast -> text = ForecastStringBuilder.buildString(airqualityForecast) })
+        print("apiResponse: $text")
     }
 
     override fun onResume() {
@@ -23,4 +37,12 @@ class MainActivity : AppCompatActivity() {
         notificationService.sendNotification(NotificationService.NotificationTypes.Pause)
     }
 
+}
+
+object ForecastStringBuilder {
+    fun buildString(forecast: List<AirqualityForecast>): String {
+        val forecastString = StringBuilder().append("Forecast:\n")
+        forecast.forEach { forecastString.append(/*Things of the api..*/"hi") }
+        return forecastString.toString()
+    }
 }
