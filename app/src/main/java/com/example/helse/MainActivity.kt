00ebaa52797a207c408abdcab4f62364b566d7e9
Bjørn Olav <bojalborg@gmail.com>
2019-03-06 16:34:12 +0100

    Remove notifications from MainActivity.

--- app/src/main/java/com/example/helse/MainActivity.kt
+++ app/src/main/java/com/example/helse/MainActivity.kt
@@ -1,47 +1,39 @@
 package com.example.helse
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.example.helse.forecast.AirqualityForecast
 import com.example.helse.forecast.AirqualityForecastApiImpl
 import com.example.helse.forecast.AirqualityForecastRepositoryImpl
 import com.example.helse.forecast.AirqualityForecastViewModel
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var notificationService: NotificationService
     private lateinit var text: String
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        notificationService = NotificationService(this)
+        setContentView(R.layout.activity_main)
+        text = "temp"
 
         val viewModel = ViewModelProviders.of(this).get(AirqualityForecastViewModel::class.java)
             .apply { airqualityForecastRepository = AirqualityForecastRepositoryImpl(AirqualityForecastApiImpl()) }
 
         viewModel.getAirqualityForecast().observe(
             this,
-            Observer { airqualityForecast -> text = ForecastStringBuilder.buildString(airqualityForecast) })
-        print("apiResponse: $text")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        notificationService.sendNotification(NotificationService.NotificationTypes.Resume)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        notificationService.sendNotification(NotificationService.NotificationTypes.Pause)
+            Observer { airqualityForecast ->
+                findViewById<TextView>(R.id.text).text = ForecastStringBuilder.buildString(airqualityForecast)
+            })
+        println("apiResponse: ${findViewById<TextView>(R.id.text).text.toString()}")
     }
 
 }
 
 object ForecastStringBuilder {
-    fun buildString(forecast: List<AirqualityForecast>): String {
+    fun buildString(forecast: List<Any>): String {
         val forecastString = StringBuilder().append("Forecast:\n")
+        println("forecast: ${forecast}")
         forecast.forEach { forecastString.append(/*Things of the api..*/"hi") }
         return forecastString.toString()
     }
