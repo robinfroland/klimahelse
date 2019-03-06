67057222943c4052c9a109b1ff08126a02e35520
Araahm97 <araahm97@live.no>
2019-03-06 15:06:48 +0100

    Fixed not working ApiRequest -MainActivity will now get JSON objects instead and parse these -RequestAPIRequest has no interface and is now AsyncTask -Gradle, App has comlieOptions (not sure why it needed these)

--- app/src/main/java/com/example/helse/Location.kt
+++ app/src/main/java/com/example/helse/Location.kt
@@ -1,3 +1,3 @@
 package com.example.helse
 
-class Location (val name: String, val kommune: String )
+class Location (val name: String, var kommune: String? =null )
--- app/src/main/java/com/example/helse/MainActivity.kt
+++ app/src/main/java/com/example/helse/MainActivity.kt
@@ -2,21 +2,40 @@ package com.example.helse
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONArray
 
 class MainActivity : AppCompatActivity() {
+
+    val locations = ArrayList<Location>()
+    private val url = "https://api.met.no/weatherapi/airqualityforecast/0.1/stations"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        fetchData(url)
     }
-}
 
-object LocationStringBuilder {
-    fun buildString(locations: List<Location>): String {
-        val locationString = StringBuilder().append("Locations:\n")
-        locations.forEach { locationString.append(it.name + " " + it.kommune + "\n") }
-        return locationString.toString()
+    fun fetchData(url: String){
+        val response= LocationApiRequest().execute(url).get()
+        val jObject = JSONArray(response)
+
+        for (i in 0 until jObject.length()) {
+            val jobject = jObject.getJSONObject(i)
+            val name = jobject.getString("name")
+            val kommuneName = jobject.getJSONObject("kommune").getString("name")
+            locations.add(Location(name, kommuneName))
+        }
+
+        text.text = LocationStringBuilder.buildString(locations)
     }
+
+    object LocationStringBuilder {
+        fun buildString(locations: List<Location>): String {
+            val locationString = StringBuilder().append("Locations:\n")
+            locations.forEach { locationString.append(it.name + ", " + it.kommune + "\n") }
+            return locationString.toString()
+        }
+    }
+
 }
