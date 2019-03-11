43317b1b2a114610dfc384a076f047e352b6a5fe
Claudia Sikora <Cloudy@1x-193-157-169-107.uio.no>
2019-03-11 13:36:06 +0100

    added search files again :(

--- app/src/main/java/com/example/helse/search/ListAdapter.kt
+++ app/src/main/java/com/example/helse/search/ListAdapter.kt
@@ -1,2 +1,91 @@
 package com.example.helse.search
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.Filter
+import android.widget.Filterable
+import java.util.ArrayList
+import android.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil
+import com.example.helse.R
+import com.example.helse.search.databinding.RowItemBinding
+
+class ListAdapter(internal var mData: List<String>) : BaseAdapter(), Filterable {
+    internal var mStringFilterList: List<String>
+    internal var valueFilter: ValueFilter? = null
+    private var inflater: LayoutInflater? = null
+
+    init {
+        mStringFilterList = mData
+    }
+
+
+    override fun getCount(): Int {
+        return mData.size
+    }
+
+    override fun getItem(position: Int): String {
+        return mData[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    @SuppressLint("ViewHolder")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        if (inflater == null) {
+            inflater = parent.context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        }
+        val rowItemBinding: RowItemBinding = DataBindingUtil.inflate(inflater!!, R.layout.row_item, parent, false)
+        rowItemBinding.stringName.setText(mData[position])
+
+
+        return rowItemBinding.getRoot()
+    }
+
+    override fun getFilter(): Filter {
+        if (valueFilter == null) {
+            valueFilter = ValueFilter()
+        }
+        return valueFilter as ValueFilter
+    }
+
+    inner class ValueFilter : Filter() {
+        override fun performFiltering(constraint: CharSequence?): Filter.FilterResults {
+            val results = Filter.FilterResults()
+
+            if (constraint != null && constraint.length > 0) {
+                val filterList = ArrayList<String>()
+                for (i in mStringFilterList.indices) {
+                    if (mStringFilterList[i].toUpperCase().contains(constraint.toString().toUpperCase())) {
+                        filterList.add(mStringFilterList[i])
+                    }
+                }
+                results.count = filterList.size
+                results.values = filterList
+            } else {
+                results.count = mStringFilterList.size
+                results.values = mStringFilterList
+            }
+            return results
+
+        }
+
+        override fun publishResults(
+            constraint: CharSequence,
+            results: Filter.FilterResults
+        ) {
+            mData = results.values as List<String>
+            notifyDataSetChanged()
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/helse/search/SearchActivity.kt
+++ app/src/main/java/com/example/helse/search/SearchActivity.kt
@@ -1,2 +1,52 @@
 package com.example.helse.search
 
+import android.databinding.DataBindingUtil.setContentView
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.SearchView
+import android.widget.ListAdapter
+import androidx.appcompat.app.AppCompatActivity
+import com.example.helse.R
+import com.example.helse.search.databinding.ActivityMainBinding
+
+import java.util.ArrayList
+
+class MainActivity : AppCompatActivity() {
+
+    internal lateinit var activityMainBinding: ActivityMainBinding
+    internal lateinit var adapter: ListAdapter
+
+    internal var searchList: MutableList<String> = ArrayList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = setContentView(this, R.layout.activity_main)
+
+        searchList.add("Oslo")
+        searchList.add("Bergen")
+        searchList.add("Stavanger")
+        searchList.add("Trondheim")
+
+        adapter = ListAdapter(searchList)
+        activityMainBinding.listView.setAdapter(adapter)
+
+        activityMainBinding.search.setActivated(true)
+        activityMainBinding.search.setQueryHint("Søk etter område")
+        activityMainBinding.search.onActionViewExpanded()
+        activityMainBinding.search.setIconified(false)
+        activityMainBinding.search.clearFocus()
+
+        activityMainBinding.search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+
+                adapter.filter.filter(newText)
+
+                return false
+            }
+        })
+    }
+}
