25e8f33d9f2256d01de4908fa99de3f65c3941b5
Robin Frøland <robinfroland@gmail.com>
2019-03-16 14:26:08 +0100

    Manually create dotindicators

--- app/src/main/java/com/example/helse/OnboardingActivity.kt
+++ app/src/main/java/com/example/helse/OnboardingActivity.kt
@@ -1,14 +1,12 @@
 package com.example.helse
 
 import android.os.Bundle
-import android.view.View
 import android.view.Window
 import android.view.WindowManager
-import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
 import com.example.helse.adapters.OnboardingAdapter
-import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_onboarding.*
 
 
@@ -19,15 +17,15 @@ class OnboardingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        setContentView(R.layout.activity_onboarding)
+        setFullscreen()
 
         onboardingAdapter = OnboardingAdapter(supportFragmentManager)
         viewPager.adapter = onboardingAdapter
 
-        dotIndicator.setupWithViewPager(viewPager)
+        updateDotIndicator(0)
+        viewPager.setOnPageListener()
+
+
 
     }
 
@@ -37,5 +35,39 @@ class OnboardingActivity : AppCompatActivity() {
         } else {
             return
         }
+    }
+
+    private fun updateDotIndicator(page: Int) {
+        when(page) {
+            0 -> {
+                firstDot.setBackgroundResource(R.drawable.dot_on_color)
+                secondDot.setBackgroundResource(R.drawable.dot_on_color)
+                secondDot.background.alpha = 128 // 50% opacity
+            }
+            1 -> {
+                firstDot.setBackgroundResource(R.drawable.dot_on_white)
+                secondDot.setBackgroundResource(R.drawable.dot_on_white)
+                firstDot.background.alpha = 128 // 50% opacity
+            }
+        }
+    }
+
+    private fun setFullscreen() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_onboarding)
+    }
+
+    private fun ViewPager.setOnPageListener() {
+        this.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageSelected(position: Int) {
+                updateDotIndicator(position)
+            }
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            }
+
+        })
     }
 }
