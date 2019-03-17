b37fa2002de8ff6cf08246b6a1a4b59370faaff4
Robin Frøland <robinfroland@gmail.com>
2019-03-17 11:33:56 +0100

    Set up Google Play services and specify permission

--- app/src/main/java/com/example/helse/OnboardingActivity.kt
+++ app/src/main/java/com/example/helse/OnboardingActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.example.helse.adapters.OnboardingAdapter
