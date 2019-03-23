61831725dd5aab4881c4b48b0f0889fd1f7cc427
Robin Frøland <robinfroland@gmail.com>
2019-03-23 16:37:01 +0100

    Create data class for modules

---
+++ app/src/main/java/com/example/helse/data/entities/ModuleCard.kt
@@ -0,0 +1,7 @@
+package com.example.helse.data.entities
+
+data class ModuleCard(
+    val category: String = "Module",
+    val dangerIndicator: String = "Dangerindicator",
+    val pushEnabled: Boolean = true
+)
