6c2d996f14124b6e1a429c24842cbb7b820571f4
Bjørn Olav <bojalborg@gmail.com>
2019-03-23 16:53:10 +0100

    Format Location/AirqualityResponse to ready for testing

--- app/src/main/java/com/example/helse/data/api/LocationResponse.kt
+++ app/src/main/java/com/example/helse/data/api/LocationResponse.kt
@@ -3,61 +3,35 @@ package com.example.helse.data.api
 import android.app.Activity
 import com.example.helse.data.entities.Location
 import com.example.helse.data.entities.emptyLocation
+import com.example.helse.utilities.parseLocationResponse
 import com.example.helse.utilities.showNetworkError
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
-import org.json.JSONArray
+
+private const val LOCATION_URL = "https://api.met.no/weatherapi/airqualityforecast/0.1/stations"
 
 interface LocationApi {
-    fun fetchAllLocations(): MutableList<Location>
+    fun fetchAllLocations(url: String = LOCATION_URL): MutableList<Location>
 }
 
 class LocationResponse(private val activity: Activity?) : LocationApi {
+    override fun fetchAllLocations(url: String): MutableList<Location> {
+        val client = OkHttpClient()
 
-    private val client = OkHttpClient()
-    private val locationUrl = "https://api.met.no/weatherapi/airqualityforecast/0.1/statio"
-
-    override fun fetchAllLocations(): MutableList<Location> {
         lateinit var response: Response
         return try {
             val request = Request.Builder()
-                .url(locationUrl)
+                .url(url)
                 .get()
                 .build()
 
             response = client.newCall(request).execute()
 
-            response.parseResponse()
+            response.parseLocationResponse()
         } catch (e: Exception) {
             showNetworkError(activity, response.code(), e)
             mutableListOf(emptyLocation)
-        }
-    }
-
-    companion object {
-        fun Response.parseResponse(): MutableList<Location> {
-            val body = JSONArray(this.body()?.string())
-            val parsedResponse = ArrayList<Location>()
-
-            for (i in 0 until body.length()) {
-                val locationObject = body.getJSONObject(i)
-                val location = locationObject.getString("name")
-                val superlocation = locationObject.getJSONObject("kommune").getString("name")
-                val longitude = locationObject.getString("longitude").toDouble()
-                val latitude = locationObject.getString("latitude").toDouble()
-                val station = locationObject.getString("eoi")
-                parsedResponse.add(
-                    Location(
-                        location,
-                        superlocation,
-                        longitude,
-                        latitude,
-                        station
-                    )
-                )
-            }
-            return parsedResponse
         }
     }
 }
