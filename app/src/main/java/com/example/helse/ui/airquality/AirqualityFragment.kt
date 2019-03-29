e086a86283e6b54d51e389649d71b7fbbcdd7f84
Bjørn Olav <bojalborg@gmail.com>
2019-03-29 11:14:18 +0100

    Autofix some formatting..

--- app/src/main/java/com/example/helse/ui/airquality/AirqualityFragment.kt
+++ app/src/main/java/com/example/helse/ui/airquality/AirqualityFragment.kt
@@ -49,10 +49,14 @@ class AirqualityFragment : Fragment() {
             }
 
         airqualityViewModel.getAirqualityForecast().observe(this, Observer { forecast ->
-            o3_concentration.text = getString(R.string.o3_concentration, forecast.o3_concentration, forecast.o3_riskValue)
-            no2_concentration.text = getString(R.string.no2_concentration, forecast.no2_concentration, forecast.no2_riskValue)
-            pm10_concentration.text = getString(R.string.pm10_concentration, forecast.pm10_concentration, forecast.pm10_riskValue)
-            pm25_concentration.text = getString(R.string.pm25_concentration, forecast.pm25_concentration, forecast.pm25_riskValue)
+            o3_concentration.text =
+                getString(R.string.o3_concentration, forecast.o3_concentration, forecast.o3_riskValue)
+            no2_concentration.text =
+                getString(R.string.no2_concentration, forecast.no2_concentration, forecast.no2_riskValue)
+            pm10_concentration.text =
+                getString(R.string.pm10_concentration, forecast.pm10_concentration, forecast.pm10_riskValue)
+            pm25_concentration.text =
+                getString(R.string.pm25_concentration, forecast.pm25_concentration, forecast.pm25_riskValue)
             risk_value.text = getString(R.string.risk_value, forecast.riskValue)
 
         })
