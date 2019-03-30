afe890ec1c11948824b3aa7d21c26e4e92470aa2
Araahm97 <araahm97@live.no>
2019-03-30 20:19:35 +0100

    Will fetch all times of the day in a MutableList

---
+++ app/src/main/java/com/example/helse/adapters/HorisontalAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.helse.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.cardview.widget.CardView
+import com.example.helse.R
+import com.example.helse.data.entities.RiskCircles
+import kotlinx.android.synthetic.main.list_item_risk.view.*
+
+
+class HorisontalAdapter(private val timeList : ArrayList<RiskCircles>) : RecyclerView.Adapter<CardViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
+
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val riskCircleLayout = layoutInflater.inflate(R.layout.list_item_risk, parent, false) as CardView
+        return CardViewHolder(riskCircleLayout)
+    }
+
+    override fun onBindViewHolder(holder: CardViewHolder, index: Int) {
+        holder.view.risk_circle.risk_text.text = timeList[index].hourOfDay.toString()
+        val riskValue = timeList[index].overallRiskValue
+        when (riskValue) {
+            "LAV RISIKO" -> holder.view.risk_circle.setBackgroundResource(R.drawable.circle_danger_low)
+            "MODERAT" -> holder.view.risk_circle.setBackgroundResource(R.drawable.circle_danger_medium)
+            "BETYDELIG RISIKO" -> holder.view.risk_circle.setBackgroundResource(R.drawable.circle_danger_high)
+            "ALVORLIG RISIKO" -> holder.view.risk_circle.setBackgroundResource(R.drawable.indicator_danger_very_high)
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return timeList.size
+    }
+
+
+}
+
+class CardViewHolder(val view: CardView) : RecyclerView.ViewHolder(view)
+
+
+
---
+++ app/src/main/java/com/example/helse/data/entities/RiskCircles.kt
@@ -0,0 +1,7 @@
+package com.example.helse.data.entities
+
+data class RiskCircles(
+    val hourOfDay : Int,
+    val overallRiskValue: String
+
+)
