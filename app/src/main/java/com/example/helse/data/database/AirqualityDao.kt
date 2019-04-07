ff8572b91a19a9bb54b9ef786450bee11d5d7233
Araahm97 <araahm97@live.no>
2019-04-07 11:40:42 +0200

    removed method from database

--- app/src/main/java/com/example/helse/data/database/AirqualityDao.kt
+++ app/src/main/java/com/example/helse/data/database/AirqualityDao.kt
@@ -8,8 +8,6 @@ import com.example.helse.data.entities.AirqualityForecast
 
 @Dao
 interface AirqualityDao {
-    @Query("SELECT * FROM airqualityForecast WHERE 'from' = :hour")
-    fun getAtHour(hour: Int ): AirqualityForecast
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(airquality: MutableList<AirqualityForecast>)
