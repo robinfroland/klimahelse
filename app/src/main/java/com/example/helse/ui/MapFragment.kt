f40bfd9d8bd1e2c8e6e4f94c33603a7e17e348d6
Bjørn Olav Jalborg <boj@arcanecrypto.no>
2019-05-06 17:58:37 +0200

    Remove spinner

--- app/src/main/java/com/example/helse/ui/MapFragment.kt
+++ app/src/main/java/com/example/helse/ui/MapFragment.kt
@@ -1,51 +1,37 @@
 package com.example.helse.ui
 
 import android.graphics.Color
-import android.media.session.MediaSession
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.example.helse.R
 import com.example.helse.data.api.AirqualityResponse
 import com.example.helse.data.api.LocationResponse
 import com.example.helse.data.database.LocalDatabase
 import com.example.helse.data.entities.AirqualityForecast
 import com.example.helse.data.entities.Location
-import com.example.helse.data.repositories.AirqualityRepositoryImpl
 import com.example.helse.data.repositories.LocationRepositoryImpl
 import com.example.helse.utilities.HIGH_AQI_VALUE
 import com.example.helse.utilities.LOW_AQI_VALUE
 import com.example.helse.utilities.MEDIUM_AQI_VALUE
 import com.example.helse.utilities.VERY_HIGH_AQI_VALUE
-import com.example.helse.viewmodels.AirqualityViewModel
-import com.example.helse.viewmodels.MapViewModel
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
-import kotlinx.android.synthetic.main.fragment_map.*
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import java.util.*
-import kotlin.coroutines.Continuation
-import kotlin.coroutines.suspendCoroutine
 
 class MapFragment : Fragment(), OnMapReadyCallback {
     private lateinit var mapView: MapView
     private lateinit var map: GoogleMap
-    private lateinit var spinner: ProgressBar
-
-    private lateinit var viewModel: MapViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -56,8 +42,6 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mapSpinner.show()
-        mapSpinner.visibility = View.VISIBLE
 
         mapView = requireActivity().findViewById(R.id.map)
         mapView.onCreate(savedInstanceState)
@@ -87,8 +71,6 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             ).getAllLocations()
 
             addAirqualityToMap(p0, locations)
-            mapSpinner.hide()
-            mapSpinner.visibility = View.GONE
         }
         val alnabru = LatLng(59.932141, 10.846132)
 
