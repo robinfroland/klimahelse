e5f27e98cc367ea6bb45cc19d46f89b882609a00
Robin Frøland <robinfroland@gmail.com>
2019-05-12 18:05:03 +0200

    Update fragment labels

--- android-app/app/src/main/java/com/example/helse/MainActivity.kt
+++ android-app/app/src/main/java/com/example/helse/MainActivity.kt
@@ -85,7 +85,6 @@ class MainActivity : AppCompatActivity(), PreferenceFragmentCompat.OnPreferenceS
             "location_settings" -> navController.navigate(R.id.settings_to_locationsettings)
             "dashboard_settings" -> navController.navigate(R.id.settings_to_dashboardsettings)
             "push_settings" -> navController.navigate(R.id.settings_to_pushsettings)
-            "push_advanced_settings" -> navController.navigate(R.id.pushsettings_to_advanced)
             "location_search_settings" -> navController.navigate(R.id.locationsettings_to_search)
         }
         return true
