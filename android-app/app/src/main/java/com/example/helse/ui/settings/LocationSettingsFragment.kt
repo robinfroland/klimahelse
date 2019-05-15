4ccf27311e513187b8987c4e7bbc42f568d6879b
Robin Frøland <robinfroland@gmail.com>
2019-05-15 12:29:44 +0200

    Remove redundant code from individual settingfragments

--- android-app/app/src/main/java/com/example/helse/ui/settings/LocationSettingsFragment.kt
+++ android-app/app/src/main/java/com/example/helse/ui/settings/LocationSettingsFragment.kt
@@ -9,10 +9,6 @@ import com.example.helse.utilities.Injector
 class LocationSettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.location_settings, rootKey)
-        val preferences = Injector.getAppPreferences(requireContext())
-        if (!preferences.isFirstLaunch()) {
-            preferenceScreen.removePreference(findPreference("PREF_DESCRIPTION"))
-        }
     }
 
     companion object {
